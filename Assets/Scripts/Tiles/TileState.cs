ba630e06a24a342a598ab55c39685ffce5c4c989
Hakkology <yildizhakan88@gmail.com>
2025-10-11 21:25:21 +0300

    2048

---
+++ Assets/Scripts/Tiles/TileState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Tile State", menuName = "Tile State", order = 0)]
+public class TileState : ScriptableObject 
+{
+    public Color backgroundColor;
+    public Color textColor;
+}
