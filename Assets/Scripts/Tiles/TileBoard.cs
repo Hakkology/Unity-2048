adf960c305748e80bd0d57da3bf130e008c92c48
Hakkology <yildizhakan88@gmail.com>
2025-10-12 06:40:40 +0300

    tiles moving

--- Assets/Scripts/Tiles/TileBoard.cs
+++ Assets/Scripts/Tiles/TileBoard.cs
@@ -6,9 +6,9 @@ public class TileBoard : MonoBehaviour {
     public Tile tilePrefab;
     public TileState[] tileStates;
 
-    
+
     TileGrid grid;
-    private List<Tile> tiles = new();
+    private List<Tile> tiles = new List<Tile>();
 
     void Awake()
     {
@@ -20,9 +20,72 @@ public class TileBoard : MonoBehaviour {
         CreateTile();
         CreateTile();
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            MoveTiles(Vector2Int.up, 0, 1, 1, 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            MoveTiles(Vector2Int.down, 0, 1, grid.height-2, -1);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveTiles(Vector2Int.left, 1, 1, 0, 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveTiles(Vector2Int.right, grid.width -2, -1, 0, 1);
+        }
+    }
+
+    void MoveTiles(Vector2Int direction, int startX, int incrementX, int startY, int incrementY)
+    {
+        for (int x = startX; x >= 0 && x < grid.width; x += incrementX)
+        {
+            for (int y = startY; y >= 0 && y < grid.height; y += incrementY)
+            {
+                TileCell cell = grid.GetCell(x, y);
+
+                if (cell.occupied)
+                {
+                    MoveTile(cell.tile, direction);
+                }
+            }
+        }
+    }
     
+    void MoveTile(Tile tile, Vector2Int direction)
+    {
+        TileCell newCell = null;
+        TileCell adjacent = grid.GetAdjacentCell(tile.cell, direction);
+
+        while (adjacent != null)
+        {
+            if (adjacent.occupied)
+            {
+                // merge yapılacak.
+                break;
+            }
+
+            newCell = adjacent;
+            adjacent = grid.GetAdjacentCell(adjacent, direction);
+        }
+
+        if (newCell != null)
+        {
+            tile.MoveTo(newCell);
+        }
+
+    }
+
     void CreateTile()
     {
         Tile tile = Instantiate(tilePrefab, grid.transform);
+        tile.SetState(tileStates[0], 2);
+        tile.Spawn(grid.GetRandomEmptyCell());
+        tiles.Add(tile);
     }
 }
--- Assets/Scripts/Tiles/TileGrid.cs
+++ Assets/Scripts/Tiles/TileGrid.cs
@@ -27,4 +27,48 @@ public class TileGrid : MonoBehaviour
             }
         }
     }
+
+    public TileCell GetCell(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+            return rows[y].cells[x];
+        else
+            return null;
+    }
+
+    public TileCell GetCell(Vector2Int coordinates)
+    {
+        return GetCell(coordinates.x, coordinates.y);
+    }
+    
+    public TileCell GetAdjacentCell(TileCell cell, Vector2Int direction)
+    {
+        Vector2Int coordinates = cell.coordinates;
+        coordinates.x += direction.x;
+        coordinates.y -= direction.y; // çünkü karakterin yukarı gitmesi kanvasta yyi düşürmek demek.
+        return GetCell(coordinates);
+    }
+
+    public TileCell GetRandomEmptyCell()
+    {
+        int index = Random.Range(0, cells.Length);
+        int startingIndex = index;
+
+        while (cells[index].occupied)
+        {
+            // eğer hücre doluysa bir sonrakine bak.
+            index++;
+            if (index >= cells.Length)
+            {
+                index = 0;
+            }
+            // eğer tüm hücreler doluysa
+            if (index == startingIndex)
+            {
+                return null;
+            }
+        }
+
+        return cells[index];
+    }
 }
